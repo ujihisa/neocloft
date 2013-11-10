70e2afbdee0cc1bc1ec19481816786a4a090e896
ujihisa <ujihisa@gmail.com>
2013-11-10 21:22:49 +0900

    chainmail guards sword damages

--- samples/chainmail_swordguard.clj
+++ samples/chainmail_swordguard.clj
@@ -1,16 +1,24 @@
 (ns neocloft.chainmail-swordguard
   (:use [neocloft.clojure-plugin :only [defh]])
-  (:import [org.bukkit Material]))
+  (:import [org.bukkit Material]
+           [org.bukkit.entity Player LivingEntity]))
 (def handler (atom {}))
 (def worlds #{"world" "world_nether" "world_end"})
 ; vim: lispwords+=defh,later :
 
+(defn- equipment [entity]
+  (condp instance? entity
+    Player (.getInventory entity)
+    LivingEntity (.getEquipment entity)
+    nil))
+
 (defh entity.EntityDamageByEntityEvent handler [evt entity]
   (let [swords #{Material/WOOD_SWORD Material/STONE_SWORD Material/IRON_SWORD
-                 Material/GOLD_SWORD Material/DIAMOND_SWORD}]
+                 Material/GOLD_SWORD Material/DIAMOND_SWORD}
+        damager (-> evt .getDamager)]
     (when (and
             (= Material/CHAINMAIL_CHESTPLATE
-               (some-> entity .getEquipment .getChestplate .getType))
-            (contains? swords (some-> evt .getDamager .getEquipment .getItemInHand)))
-      (.setCancelled evt true)
-      (prn 'ok))))
+               (some-> entity equipment .getChestplate .getType))
+            (contains? swords (some-> damager equipment .getItemInHand .getType)))
+      (.setDamage evt 0)
+      (.damage damager 0 entity))))
