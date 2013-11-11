03928f88853830b6820dfd7350cce88493fbb948
ujihisa <ujihisa@gmail.com>
2013-11-11 21:06:35 +0900

    heartbreaking: Blaze drops obsidian 1/10

--- samples/heartbreaking.clj
+++ samples/heartbreaking.clj
@@ -1,11 +1,22 @@
 (ns neocloft.heartbreaking
   (:use [neocloft.clojure-plugin :only [defh]])
-  (:import [org.bukkit Material]
-           [org.bukkit.entity Player LivingEntity]))
+  (:import [org.bukkit Material Bukkit]
+           [org.bukkit.entity Player LivingEntity Blaze]))
 (def handler (atom {}))
 (def worlds #{"world"})
-; vim: lispwords+=defh,later :
 
 (defh player.PlayerRespawnEvent handler [evt player]
   (let [loc (.getSpawnLocation (Bukkit/getWorld "world_nether"))]
     (.teleport player loc)))
+
+(defn -drop-item [loc istack]
+  (.dropItemNaturally (.getWorld loc) loc istack))
+
+(defh entity.EntityDeathEvent handler [evt entity]
+  (condp instance? entity
+    Blaze
+    (when (= 0 (rand-int 10))
+      (drop-item (.getLocation entity) (ItemStack. Material/OBSIDIAN 1)))
+    nil))
+
+; vim: lispwords+=defh,later :
