16bee57535e5ad990b5670251080bbfcba7412eb
ujihisa <ujihisa@gmail.com>
2013-11-30 15:36:37 -0800

    add a dummy failing test for jenkins

---
+++ test/neocloft/clojure_plugin_test.clj
@@ -0,0 +1,7 @@
+(ns neocloft.clojure-plugin-test
+  (:require [clojure.test :refer :all]
+            [neocloft.clojure-plugin :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
