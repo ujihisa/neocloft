14a6e5f8396aa364b405c6d2dfa9e6805e6d9716
ujihisa <ujihisa@gmail.com>
2013-12-01 13:41:42 -0800

    core.match

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.bukkit/bukkit "1.7.2-R0.1-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  #_[org.jruby/jruby-complete "1.7.6"]
-                 [org.clojure/tools.nrepl "0.2.3"]]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.clojure/core.match "0.2.0"]]
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :repositories {"org.bukkit"
