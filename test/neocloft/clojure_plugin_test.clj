e2caeb4e929d24a032fec0df4a85007d0bc87e53
ujihisa <ujihisa@gmail.com>
2013-11-30 20:18:38 -0800

    add test for sec

--- test/neocloft/clojure_plugin_test.clj
+++ test/neocloft/clojure_plugin_test.clj
@@ -5,3 +5,7 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest sec-tect
+  (testing "converts from second unit to tick unit (a tick is 1/20 sec)"
+    (is (= 60 (sec 3.0)))))
