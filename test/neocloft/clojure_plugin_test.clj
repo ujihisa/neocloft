27db12ecd866cc67e2379ed69c7123c011012126
ujihisa <ujihisa@gmail.com>
2013-12-02 00:52:56 -0800

    no failure test for testing

--- test/neocloft/clojure_plugin_test.clj
+++ test/neocloft/clojure_plugin_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [neocloft.clojure-plugin :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (deftest sec-tect
   (testing "converts from second unit to tick unit (a tick is 1/20 sec)"
     (is (= 60 (sec 3.0)))))
