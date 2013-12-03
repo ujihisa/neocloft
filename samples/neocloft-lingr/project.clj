49655be7a61217d0cf99b4dc015583b3fefab4dc
ujihisa <ujihisa@gmail.com>
2013-12-02 21:52:58 -0800

    sketch of neocloft-lingr, using compojure

--- samples/neocloft-lingr/project.clj
+++ samples/neocloft-lingr/project.clj
@@ -3,11 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :profiles {:dev
-             {:repositories {"org.bukkit"
-                             "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
-              :dependencies [[org.clojure/clojure "1.5.1"]
-                             [org.bukkit/bukkit "1.7.2-R0.1-SNAPSHOT"]
-                             [neocloft "1.0.0-SNAPSHOT"]]}}
+  :profiles
+  {:dev
+   {:repositories {"org.bukkit"
+                   "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
+   :dependencies [[org.clojure/clojure "1.5.1"]
+                  [org.bukkit/bukkit "1.7.2-R0.1-SNAPSHOT"]
+                  [neocloft "1.0.0-SNAPSHOT"]
+                  [compojure "1.1.6"]
+                  [ring/ring-jetty-adapter "1.2.1"]]}}
   :dependencies []
   :aot :all)
--- samples/neocloft-lingr/src/neocloft_lingr/core.clj
+++ samples/neocloft-lingr/src/neocloft_lingr/core.clj
@@ -1,9 +1,37 @@
 (ns neocloft-lingr.core
-  (:use [neocloft.clojure-plugin :only [defh later sec]])
-  (:require [neocloft.helper :as helper])
-  (:import [org.bukkit Material]))
+  (:use [neocloft.clojure-plugin :only [defh later sec]]
+        [compojure.core :only (defroutes GET POST ANY)]
+        #_[clojure.data.json :only (read-json)]
+        [ring.adapter.jetty :only (run-jetty)])
+  (:require [neocloft.helper :as helper]
+            [clojure.string :as s])
+  (:import [org.bukkit Material])
+  (:gen-class))
 (def handler (atom {}))
 (def worlds #{"world"})
 
-(defn -main []
-  (prn 'ok))
+(defn handle-post [body]
+  (prn 'handle-post body))
+
+(defroutes routes
+  (let [start-time (java.util.Date.)]
+    (GET "/" []
+         (str {:homepage "https://github.com/ujihisa/neocloft"
+               :from start-time
+               :author "ujihisa"})))
+  (POST "/" {body :body}
+        (s/join "\n" (handle-post body)))
+  #_(POST "/dev" {body :body headers :headers}
+    (when (#{"64.46.24.16"} (headers "x-forwarded-for"))
+      (my-safe-eval (slurp body))
+      #_(let [body-parsed (try
+                          (read-string (slurp body))
+                          (catch RuntimeException e e))]
+        (my-safe-eval (get body-parsed "code" nil)))))
+  #_(ANY "*" {body :body headers :headers}
+    (any-handler body headers)))
+
+(defn on-enable [plugin]
+  (let [port (-> plugin .getConfig (.getLong "neocloft-lingr.port" 8126))]
+    (prn 'starting 'neocloft-lingr :port port)
+    (run-jetty routes {:port port :join? false})))
