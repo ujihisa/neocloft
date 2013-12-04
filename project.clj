d17b3cfb24d3346bd16efced240ea02a13d5811a
ujihisa <ujihisa@gmail.com>
2013-12-03 21:49:24 -0800

    Add apache httpclient as dependency explicitly even though it's verbose. This is tricky. just to disable legacy httpclient-4.1.2 for neocloft apps to be able to use buggy clj-http

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
                  #_[org.jruby/jruby-complete "1.7.6"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/core.match "0.2.0"]
-                 [com.cemerick/pomegranate "0.2.0"]]
+                 [com.cemerick/pomegranate "0.2.0"]
+                 ; This is tricky. just to disable legacy httpclient-4.1.2 for neocloft apps to be able to use buggy clj-http
+                 [org.apache.httpcomponents/httpclient "4.3"]]
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :repositories {"org.bukkit"
